8313c4a5db7a31a87ec35513841aa9f75dd7b2d3
Ray-Palmer <sudeerachandrajith1212@gmail.com>
2023-01-18 03:17:33 +0530

    added

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,5 +1,8 @@
 import "./Card.css";
 
-const Card = () => {
-  return <div ClassName='card'></div>;
+const Card = (props) => {
+  const classes = "card" + props.className;
+  return <div ClassName={classes}>{props.children}</div>;
 };
+
+export default Card;
